f19031c077fa5727eef29a6c651ba8e75c6f8755
Keagan Peterson <Keagan.a.peterson@outlook.com>
2023-02-15 18:03:56 -0600

    second commit

--- Credentials.py
+++ Credentials.py
@@ -1,23 +1,23 @@
 def username():
-    textnow_username = "keagan.a.peterson"
+    textnow_username = ""
     return textnow_username
 
 
 def sid():
-    sid = "s%3Auc6ML5N8SFVGLdNV1J0qw7Yyu51l-DuN.xzwTHa1MttYdHCTP7Iz%2BSWqZLK7Zj0Jh6hniR4s0MqU"
+    sid = ""
     return sid
 
 
 def csrf():
-    csrf = "s%3Arn_6VbYl5W155C_pAYIQYKps.4QSJ5b3FtRmwnDp%2Ff6GHMKcju9JzbW56AesSCuxeTpE"
+    csrf = ""
     return csrf
 
 
 def weather_key():
-    open_weathermap_key = "1f8498605a482f805295d7b502e1ccf7"
+    open_weathermap_key = ""
     return open_weathermap_key
 
 
 def admin_password():
-    password = "ADMIN1234"
+    password = ""
     return password
