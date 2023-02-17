6a0a15e5dacebf729cf71b68c4d4e891e181c95c
Kasherpete <119694008+Kasherpete@users.noreply.github.com>
2023-02-16 19:18:32 -0600

    Update Command_Details.py

--- commands/Command_Details.py
+++ commands/Command_Details.py
@@ -11,4 +11,4 @@ client = pytn.Client(username, sid_cookie=sid, csrf_cookie=csrf)
 
 # TODO: update version number when making new release on GitHub
 async def details_command(msg):
-    msg.send_sms("This service is being hosted and developed by @Kasherpete#2661, @Jopes#6969 <--(Idiot), and @SpaceSaver2000#2992. Message them on discord for any questions/feedback. Thanks to SpaceSaver for hosting the service, Kasherpete for adding ChatGPT, and jopes for having the original idea, this would not be possible without their help. Version 1.3.3")
+    msg.send_sms("This service is being hosted and developed by @Kasherpete#2661, @Jopes#6969 <--(Idiot), and @SpaceSaver2000#2992. Message them on discord for any questions/feedback. Thanks to SpaceSaver for hosting the service, Kasherpete for adding ChatGPT, and jopes for having the original idea, this would not be possible without their help. Version 1.3.31")
