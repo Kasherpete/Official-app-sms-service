a785ed392d2aa9efb7f07a3c876c412d611b2b9f
Keagan Peterson <Keagan.a.peterson@outlook.com>
2023-02-24 14:47:57 -0600

    Added qr code reader

--- Custom_Message_protocols.py
+++ Custom_Message_protocols.py
@@ -1,4 +1,3 @@
-import time
 import Main
 import pytextnow as pytn
 import Credentials
@@ -59,3 +58,31 @@ def send_sms(content, msg):
                 "ERROR:INVALID_CHAR. Sorry, there was an error sending a message. This is a known bug and is currently being worked on.")
             msg.send_sms(
                 "ERROR:INVALID_CHAR. Sorry, there was an error sending a message. This is a known bug and is currently being worked on.")
+
+
+# this function is a copy of the sms.ask, but it returns the message class instead of str
+
+async def ask_advanced(question, msg, timeout, default):
+
+    timer_timeout = time.perf_counter()
+    msg.send_sms(question)
+
+    while time.perf_counter() - timer_timeout <= timeout:
+
+        await asyncio.sleep(1)
+        new_messages = Main.client.get_unread_messages()
+
+        for message in new_messages:
+
+            if message.number == msg.number:
+                message.mark_as_read()
+                return message  # <-- here is the difference
+
+    # timeout error messages
+
+    await asyncio.sleep(1)
+    if default != "":
+        msg.send_sms(f'ERROR:TIMEOUT. User took too long to respond. Default response: {default}.')
+    else:
+        msg.send_sms("ERROR:TIMEOUT. User took too long to respond. Please use command again to retry.")
+    return default
--- commands/Command_QR.py
+++ commands/Command_QR.py
@@ -2,6 +2,7 @@ import requests
 import Custom_Message_protocols as sms
 import Credentials
 import pytextnow as pytn
+import json
 
 # client initialization
 
@@ -12,7 +13,15 @@ client = pytn.Client(username, sid_cookie=sid, csrf_cookie=csrf)
 
 
 async def qr_command(msg):
-    user_response = await sms.ask("Input the data you would like a qr code for. (If enter a website, remember to add https:// before)", msg, 60, "")
+    user_response = await sms.ask("Would you like to create or read a qr code? Respond with 1 to create, and 2 to read.", msg, 60, "1")
+    if user_response == "1":
+        await qr_generate_command(msg)
+    elif user_response == "2":
+        await qr_read_command(msg)
+
+
+async def qr_generate_command(msg):
+    user_response = await sms.ask("Input the data you would like a qr code for. (If enter a website, remember to add https:// before)", msg, 60, "https://google.com")
 
     # get server response
 
@@ -25,3 +34,29 @@ async def qr_command(msg):
 
     file_path = "loadimage.png"
     client.send_mms(msg.number, file_path)
+
+
+async def qr_read_command(msg):
+
+    user_response = await sms.ask_advanced("Send your picture now.", msg, 60, "")
+
+    if user_response.type == 1:  # if message is mms
+
+        # download the file sent by user
+
+        file = f"download.{user_response.extension}"
+        user_response.mv(file)
+
+        # http request
+
+        url = 'http://api.qrserver.com/v1/read-qr-code/'
+        myfiles = {'file': open(file, 'rb')}
+        x = requests.post(url, files=myfiles)
+
+        # parse and send response
+
+        x = json.loads(x.text)
+        x = x[0]["symbol"][0]["data"]
+        sms.send_sms(x, msg)
+    else:  # if message is sms
+        msg.send_sms("ERROR:INVALID_TYPE. This error occurred because you did not respond with an image.")
